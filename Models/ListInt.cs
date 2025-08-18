13e6eab0ad749a53de26fa498b6d2468ce2b21a1
dilshad134 <121261524+dilshad134@users.noreply.github.com>
2025-08-18 09:09:36 +0400

    first commit

---
+++ Models/ListInt.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerTask.Models
+{
+    internal class ListInt
+    {
+
+        private int[] arr;
+
+
+
+
+        public int this[int index]
+        {
+            get
+            {
+
+                return arr[index];
+
+            }
+            set
+            {
+                arr[index] = value;
+            }
+
+        }
+        public ListInt()
+        {
+            arr = new int[0];
+        }
+        public ListInt(int length)
+        {
+            arr = new int[length];
+        }
+
+        public ListInt(params int[] numbers)
+        {
+            arr = numbers;
+        }
+        public int Length
+        {
+            get
+            {
+                return arr.Length;
+            }
+        }
+
+        public void Add(int num)
+        {
+            Array.Resize(ref arr, Length + 1);
+            arr[arr.Length - 1] = num;
+        }
+        public void AddRange(params int[] numbers)
+        {
+            int[] newArr = new int[numbers.Length + Length];
+            for (int i = 0; i < Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newArr[Length + i] = numbers[i];
+            }
+            arr = newArr;
+        }
+        public bool Contains(int num)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public int Sum()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        public void Remove(int num)
+        {
+            int j = 0;
+            int[] newArr = new int[Length - 1];
+            for (int i = 0; i < Length; i++)
+            {
+                if (arr[i] != num)
+                    newArr[j++] = arr[i];
+            }
+            arr = newArr;
+        }
+        public void RemoveRange(params int[] nums)
+        {
+            foreach (int num in nums)
+            {
+
+                Remove(num);
+
+            }
+
+        }
+        public override string ToString()
+        {
+            return string.Join(",", arr);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using IndexerTask.Models;
+
+namespace IndexerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListInt list = new ListInt(1, 2, 3, 4, 5);
+            Console.WriteLine(list);
+
+            list.Add(6);
+            Console.WriteLine(list);
+
+            list.AddRange(7, 8, 9);
+            Console.WriteLine(list);
+
+            //Console.WriteLine(list.Contains(5));
+            //Console.WriteLine(list.Sum());
+
+            //list.Remove(3);
+            //Console.WriteLine(list); 
+
+            list.RemoveRange(1, 9, 7);
+            Console.WriteLine(list);
+
+        }
+    }
+}
